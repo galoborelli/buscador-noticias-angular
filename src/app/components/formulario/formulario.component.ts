b44fc82b3e9c6cb559f7834689d7afe42dd10c76
galoborelli <borelligalo19@gmail.com>
2023-05-04 11:15:27 -0300

    first commit

---
+++ src/app/components/formulario/formulario.component.ts
@@ -0,0 +1,52 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario',
+  templateUrl: './formulario.component.html',
+  styleUrls: ['./formulario.component.css']
+})
+export class FormularioComponent {
+
+@Output() parametrosSeleccionados = new EventEmitter<any>()
+
+
+categoriaSeleccionada = 'general'
+paisSeleccionado = 'ar'
+
+categorias:any[] = [
+  {value:'general', nombre:'General'},
+  {value:'business', nombre:'Negocios'},
+  {value:'entertainment', nombre:'Entretenimiento'},
+  {value:'health', nombre:'Salud'},
+  {value:'science', nombre:'Ciencia'},
+  {value:'sports', nombre:'Deportes'},
+
+
+];
+
+paises:any[] = [
+  {value:'ar' , nombre:'Argentina'},
+  {value:'br' , nombre:'Brasil'},
+  {value:'fr', nombre:'Francia'},
+  {value:'hu', nombre:'Hungria'},
+  {value:'mx', nombre:'Mexico'},
+  {value:'gb', nombre:'Reino Unido'},
+
+
+];
+
+
+constructor(){
+
+}
+
+buscarNoticia(){
+  console.log(this.categoriaSeleccionada)
+ const PARAMETROS = {
+   categoria: this.categoriaSeleccionada,
+   pais: this.paisSeleccionado,
+ }
+ this.parametrosSeleccionados.emit(PARAMETROS)
+}
+
+}
---
+++ src/app/services/noticia.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NoticiaService {
+
+  constructor(private http: HttpClient) {}
+
+  getNoticias(parametros:any): Observable<any>{
+    const URL =  `https://newsapi.org/v2/top-headlines?country=${parametros.pais}&category=${parametros.categoria}&apiKey=cd18a842f7eb4cdc9bdecc3ff469510d `
+    return this.http.get(URL)
+  }
+}
